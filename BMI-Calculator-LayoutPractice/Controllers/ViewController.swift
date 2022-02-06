597e19887b6e0efa853553a597da1cd8c12c5ab7
Alicia Windsor <aliwindsor@icloud.com>
2022-02-06 22:39:59 +0000

    calculate button implemented

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -12,33 +12,37 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
-    var heightVal = "1.5"
-    var weightVal = "100"
+    //slider values stored here
+    @IBOutlet weak var height: UISlider!
+    @IBOutlet weak var weight: UISlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        updateUI()
+      
     }
     
     @IBAction func heightSliderChanged(_ sender: UISlider) {
-        heightVal = String(format: "%.2f", sender.value)
-        print(String(format: "%.2f", sender.value)) //rounding to 2dp
-        updateUI()
+        heightLabel.text = String(format: "%.2f", sender.value)
+
+        //print(String(format: "%.2f", sender.value))
+        //rounding to 2dp
+     
     }
     
     @IBAction func weightSliderChanged(_ sender: UISlider) {
         
-        weightVal = String(format: "%.0f", sender.value)
-        //print(round(sender.value * 100)/100.0)//another method for 2dp
-        print(Int(sender.value)) //no decimals
-        updateUI()
+        weightLabel.text = String(format: "%.0f", sender.value)
+        
+        //print(round(sender.value * 100)/100.0)
+        //another method for 2dp
+        //print(Int(sender.value)) //no decimals
     }
     
-    func updateUI (){
-        heightLabel.text = "\(heightVal) m"
-        weightLabel.text = "\(weightVal) kg"
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        print(weight.value/sqrt(height.value))
     }
+    
 
    
 
