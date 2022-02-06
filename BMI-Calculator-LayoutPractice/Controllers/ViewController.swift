3c2acdc1b8ca19a3eb41e2cf1cdaf6747de58c18
Alicia Windsor <aliwindsor@icloud.com>
2022-02-06 22:28:33 +0000

    sliders implemented + label update with slider change

--- BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
+++ BMI-Calculator-LayoutPractice/Controllers/ViewController.swift
@@ -9,11 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    var heightVal = "1.5"
+    var weightVal = "100"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        updateUI()
     }
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightVal = String(format: "%.2f", sender.value)
+        print(String(format: "%.2f", sender.value)) //rounding to 2dp
+        updateUI()
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        
+        weightVal = String(format: "%.0f", sender.value)
+        //print(round(sender.value * 100)/100.0)//another method for 2dp
+        print(Int(sender.value)) //no decimals
+        updateUI()
+    }
+    
+    func updateUI (){
+        heightLabel.text = "\(heightVal) m"
+        weightLabel.text = "\(weightVal) kg"
+    }
+
+   
 
 
 }
